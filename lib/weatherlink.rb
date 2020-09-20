82eb5e47140b9c42431b46d84d9259c6b9be5dcf
Jeremy Cole <jeremy@jcole.us>
2020-09-20 00:09:32 -0700

    Initial bootstrapped gem

---
+++ lib/weatherlink.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require 'weatherlink/version'
+
+module WeatherLink
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/weatherlink/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module WeatherLink
+  VERSION = '0.1.0'
+end
